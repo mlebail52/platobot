7412400544467363cbaedd4062bb9a3ef4bb2dfb
Michael Le Bail <michael.lebail@enovea.net>
2017-07-26 00:47:16 +0200

    help

--- app.js
+++ app.js
@@ -27,3 +27,15 @@ app.post('/hello', function (req, res, next) {
   }
 });
 
+app.post('/help', function (req, res, next) {
+  var userName = req.body.user_name;
+  var botPayload = {
+    text : 'What\'s wrong ' + userName + ', how can I help you?'
+  };
+  // Loop otherwise..
+  if (userName !== 'slackbot') {
+    return res.status(200).json(botPayload);
+  } else {
+    return res.status(200).end();
+  }
+});
